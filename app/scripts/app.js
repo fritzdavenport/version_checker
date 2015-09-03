0981f9a0667c1233dc89129b298d9218b35c8756
Fritz Davenport <Fritz.Davenport@dealertrack.com>
2015-09-03 12:53:27 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular
+  .module( 'yourApp', [
+    'ngResource',
+    'ui.router'
+  ] )
+  .config( [ '$stateProvider', '$urlRouterProvider',
+    function ( $stateProvider, $urlRouterProvider ) {
+
+      //delete $httpProvider.defaults.headers.common[ 'X-Requested-With' ];
+      $urlRouterProvider.otherwise( '/' );
+      $stateProvider
+        .state( 'index', {
+          url: '/',
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        } )
+        .state( 'resource', {
+          url: '/resource',
+          templateUrl: 'views/resource.html',
+          controller: 'MainCtrl'
+        } );
+
+    }
+  ] );
---
+++ app/scripts/controllers/resource.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loopbackAngularUiRouterApp.controller:ResourceCtrl
+ * @description
+ * # ResourceCtrl
+ * Controller of the loopbackAngularUiRouterApp
+ */
+angular.module('loopbackAngularUiRouterApp')
+  .controller('ResourceCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
